51c5df13dfc923b73c5b99b112441a6235797d04
Mateus Mourão <mmdfmateus@gmail.com>
2019-03-17 20:17:40 -0300

    cleaned the content fetched from Wikipedia and breaked into sentences

--- robots/text.js
+++ robots/text.js
@@ -1,16 +1,54 @@
 const credentials = require('../credentials.json')
 const algorithmiaClient = require('algorithmia')
+const sbd = require('sbd')
 
-async function robot(content){
+async function robot(content) {
     await fetchContentFromWikipedia(content)
+    cleanContent(content)
+    breakContentIntoSentences(content)
 
-    async function fetchContentFromWikipedia(content){
+    async function fetchContentFromWikipedia(content) {
         var algorithmia = algorithmiaClient(credentials.algorithmia.apiKey)
         var wikiClient = algorithmia.algo('web/WikipediaParser/0.1.2')
-        var wikiResponse = await wikiClient.pipe(content.searchTerm)
+        var wikiResponse = await wikiClient.pipe(content.search.term)
         var wikiContent = wikiResponse.get()
-        content.rawContent = wikiContent.content
-        console.log(content)
+        content.response.rawContent = wikiContent.content
+    }
+
+
+    function cleanContent(content) {
+        content.response.cleanContent = removeMarkdownAndBlankLines(content.response.rawContent)
+        content.response.cleanContent = removeDates(content.response.cleanContent)
+
+        function removeMarkdownAndBlankLines(rawContent) {
+            var lines = rawContent.split('\n')
+
+            var cleanLines = lines.filter((line) => {
+                if (line.trim().length === 0 || line.trim().startsWith('=')) {
+                    return false
+                }
+                return true
+            })
+
+            return cleanLines.join(' ')
+        }
+
+        function removeDates(rawContent) {
+            return rawContent.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ')
+        }
+    }
+
+    function breakContentIntoSentences(content) {
+        content.response.sentences = []
+
+        var sentences = sbd.sentences(content.response.cleanContent)
+        sentences.forEach((sentence) => {
+            content.response.sentences.push({
+                text: sentence,
+                keywords: [],
+                images: []
+            })
+        })
     }
 }
 
